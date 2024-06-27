04ef874f88f46ae05e180335128f9f5c723a342f
lndsilva <lndsilva@hotmail.com>
2024-06-27 19:18:56 -0300

    exemplo 01

---
+++ helloworld.js
@@ -0,0 +1,27 @@
+const http = require("http");
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  const path = req.url.replace(/\/?(?:\?.*)?$/, "").toLowerCase();
+  switch (path) {
+    case "":
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end("Homepage");
+      break;
+    case "/about":
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end("About");
+      break;
+      break;
+    default:
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("Not found");
+      break;
+  }
+});
+
+server.listen(port, () =>
+  console.log(
+    `server started on port ${port}` + ` press CTRL-C to terminate...`
+  )
+);
